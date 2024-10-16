8f44cd82e0d4067ac1392d3233c3ea4cbcbbe7ce
Weiner Oliveira <woliveira82@gmail.com>
2024-10-16 22:41:00 +0100

    feat: input reader

--- Assets/Scripts/InputReader.cs
+++ Assets/Scripts/InputReader.cs
@@ -1,9 +1,21 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Shmup
 {
+    [RequireComponent(typeof(PlayerInput))]
+
     public class InputReader : MonoBehaviour
     {
+        PlayerInput playerInput;
+        InputAction moveAction;
+
+        public Vector2 Move => moveAction.ReadValue<Vector2>();
+
+        void Start() {
+            playerInput = GetComponent<PlayerInput>();
+            moveAction = playerInput.actions["Move"];
+        }
 
     }
 
