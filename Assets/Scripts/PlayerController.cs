01c65836f483f531b5c0e928a5ea75be97137b03
Weiner Oliveira <woliveira82@gmail.com>
2024-10-17 18:46:05 +0100

    feat: player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,49 @@ namespace Shmup
 {
     public class PlayerController : MonoBehaviour
     {
+        [SerializeField] float speed = 5f;
+        [SerializeField] float smoothness = .1f;
+        [SerializeField] float leanAngle = 15f;
+        [SerializeField] float leanSpeed = 5f;
 
+        [SerializeField]  GameObject model;
+
+        [Header("Camera Bounds")]
+        [SerializeField] Transform cameraFollow;
+
+        [SerializeField] float minX = -8f;
+        [SerializeField] float maxX = 8f;
+        [SerializeField] float minY = -4f;
+        [SerializeField] float maxY = 4f;
+
+        InputReader input;
+
+        Vector3 currentVelocity;
+        Vector3 targetPosition;
+
+        void Start() {
+            input = GetComponent<InputReader>();
+        }
+
+        void Update(){
+            targetPosition += new Vector3(input.Move.x, input.Move.y, 0f) * speed * Time.deltaTime;
+
+            var minPlayerX = cameraFollow.position.x + minX;
+            var maxPlayerX = cameraFollow.position.x + maxX;
+            var minPlayerY = cameraFollow.position.y + minY;
+            var maxPlayerY = cameraFollow.position.y + maxY;
+
+            targetPosition.x = Mathf.Clamp(targetPosition.x, minPlayerX, maxPlayerX);
+            targetPosition.y = Mathf.Clamp(targetPosition.y, minPlayerY, maxPlayerY);
+
+            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref currentVelocity, smoothness);
+
+            var targetRotationAngle = -input.Move.x * leanAngle;
+
+            var currentYRotation = transform.localEulerAngles.y;
+            var newYRotation = Mathf.LerpAngle(currentYRotation, targetRotationAngle, leanSpeed * Time.deltaTime);
+
+            transform.localEulerAngles = new Vector3(0f, newYRotation, 0f);
+        }
     }
-
 }
