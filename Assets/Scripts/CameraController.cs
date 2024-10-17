ebb578b4e70b9a8a73e837663dbe9de5d48e0c24
Weiner Oliveira <woliveira82@gmail.com>
2024-10-17 17:00:33 +0100

    feat: camera controller

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,10 +1,18 @@
+using System;
 using UnityEngine;
 
 namespace Shmup
 {
     public class CameraController : MonoBehaviour
     {
+        [SerializeField] Transform player;
+        [SerializeField] float speed = 2f;
 
+        void Start() => transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+
+        void LateUpdate(){
+            transform.position += Vector3.up * speed * Time.deltaTime;
+        }
     }
 
 }
